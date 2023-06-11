6b6dc87ff06df1307a37dd4609d7d9eb4dee0520
Pranjal Shrivastava <pranjaltvds8@gmail.com>
2023-06-11 18:03:57 +0530

    Initial commit

--- src/About.js
+++ src/About.js
@@ -2,9 +2,9 @@ function AboutPage() {
     return (
         <div className="about">
             <h1>About Us</h1>
-            <p>Our history...</p>
-            <p>Our mission...</p>
-            <p>Our team...</p>
+            <p>Eat-in Restaurant is a complete family restaurant rooted in the lovely city of Bhavnagar for 23 years & counting. A spacious place, where you can make memories over delicious food, and also celebrate special occasions with your loved ones.  </p>
+            <p>Eat-in is dedicated to provide its customers with the most mouth-watering taste in the town along with taking the high hygienic measurements in preparing the food</p>
+            <p>We comprise of 5 best chefs in town, actively functioning management team and dedicated helping staff</p>
         </div>
     );
 }
--- src/HomePage.js
+++ src/HomePage.js
@@ -2,8 +2,9 @@ function HomePage() {
 
     return (
         <div style={{textAlign: 'center'}}>
-            <h1 style={{color: 'blue'}}>Welcome to our Restaurant</h1>
-            <p style={{fontSize: '20px'}}>Introductory text goes here...</p>
+            <h1 style={{color: 'blue'}}>Welcome to Eat-in Restaurant</h1>
+            <p style={{fontSize: '20px'}}>A complete family restaurant where every dish is prepared using quality ingredients, and we regularly take necessary cleanliness steps.</p>
+            <img src="restaurant-image.jpg" alt="Restaurant Image" width="400"></img>
         </div>
     );
 
